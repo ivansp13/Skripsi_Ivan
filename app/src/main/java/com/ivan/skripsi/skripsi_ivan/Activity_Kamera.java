4c98fa98ce8723a2fdf935a21af872680ebffd10
ivansp13 <vanz.tkp@gmail.com>
2018-07-31 13:44:06 +0700

    Camera udah bisa tinggal save nya belum.... Galeri & Tentang udah jalan juga tinggal yang METODE

--- app/src/main/java/com/ivan/skripsi/skripsi_ivan/Activity_Kamera.java
+++ app/src/main/java/com/ivan/skripsi/skripsi_ivan/Activity_Kamera.java
@@ -1,13 +1,57 @@
 package com.ivan.skripsi.skripsi_ivan;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class Activity_Kamera extends AppCompatActivity {
+import java.io.File;
+
+public class Activity_Kamera extends Activity {
+
+    Button button;
+    ImageView imageView;
+    static final int CAM_REQUEST = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__kamera);
+        button = (Button) findViewById(R.id.buka_kamera);
+        imageView = (ImageView) findViewById(R.id.image_camera_view);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,0);
+            }
+        });
+
+        /*
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File file = getFile();
+                camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+                startActivityForResult(camera_intent,CAM_REQUEST);
+            }
+        }); */
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
+    }
+
 }
+
